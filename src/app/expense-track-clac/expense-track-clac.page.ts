12403d931f4399acf277dcbaed40721de6d43d16
Eki <35243532+Ekidana@users.noreply.github.com>
2019-12-23 21:51:43 +0800

    added  dialogue in.ts and add ion-swipe in html

--- src/app/expense-track-clac/expense-track-clac.page.ts
+++ src/app/expense-track-clac/expense-track-clac.page.ts
@@ -8,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class ExpenseTrackClacPage implements OnInit {
 
   items=["CyberTruck","MacPro","Falcon9","E63 Wagon","A7", "PoleStar1", "PoleStar2", "Jeff Bezos", "Bill Gates","Elon Musk", "Panda Express", "Yum Foods", "Warren Buffet"];
- 
+ test(){
+   console.log ("it works!");
+ }
+delete(i){
+  console.log("Delete works!")
+  this.items.splice(i,1)
+}
   constructor() { }
 
   ngOnInit() {
